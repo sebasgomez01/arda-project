45bf119d063146148450108ed5cd5da8ad760b98
sebasgomez01 <sebasgomezp01@gmail.com>
2024-07-20 09:49:22 +0000

    agrego estructura deel proyecto.

---
+++ arda/src/main/java/com/csgp/arda/ArdaApplication.java
@@ -0,0 +1,13 @@
+package com.csgp.arda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdaApplication.class, args);
+	}
+
+}
