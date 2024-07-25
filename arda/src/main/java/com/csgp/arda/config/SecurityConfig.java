e2f891109c27a6f11761643980b654daa6fd3bc2
sebasgomez01 <sebasgomezp01@gmail.com>
2024-07-25 14:42:25 +0000

    Revert "Agrego AuthController.java para manejar la autenticación y todo lo necesario en SecurityConfig.jaa. Autentitación con JWT lista y funcionando para el usuario de prueba guardado en memoria, falta extenderlo para que funcione con los usuarios de la base de datos."
    This reverts commit 33a2e312c2d1c19f7c4fe38221adee07655af249.

--- arda/src/main/java/com/csgp/arda/config/SecurityConfig.java
+++ arda/src/main/java/com/csgp/arda/config/SecurityConfig.java
@@ -76,12 +76,5 @@ public class SecurityConfig {
                         .authorities("read")
                         .build()
         );
-    }
-
-    @Bean 
-    JwtEncoder JwtEncoder() {
-        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
-        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new NimbusJwtEncoder(jwks);
-    }
+}
 }
