81c11530189a46a90cb2e7e359df39efa4ff0fe4
sebasgomez01 <sebasgomezp01@gmail.com>
2024-07-26 11:12:33 +0000

    Empiezo el proceso de Testing. Agrego UserRepositoryTest.java para testear el repositorio de User. Error al correr los test, causa desconocida.

--- arda/src/test/java/com/csgp/arda/ArdaApplicationTests.java
+++ arda/src/test/java/com/csgp/arda/ArdaApplicationTests.java
@@ -3,11 +3,24 @@ package com.csgp.arda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.csgp.arda.web.PostController;
+
 @SpringBootTest
 class ArdaApplicationTests {
 
+	@Autowired
+	private PostController controller;
+
 	@Test
+
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+
 	}
 
 }
---
+++ arda/src/test/java/com/csgp/arda/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.csgp.arda;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.csgp.arda.domain.User;
+import com.csgp.arda.domain.UserRepository;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    
+    //@Autowired
+    private final UserRepository repository;
+
+    public UserRepositoryTest(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Test
+    void saveUser() {
+        repository.save(new User("Lucy", "paltaAguada", "abcdef", "user"));
+        //boolean value = repository.findByName("Lucy").isPresent();
+        assertThat(repository.findByName("Lucy")).isNotEmpty();
+    }
+
+    @Test
+    void deleteOwners() {
+        repository.save(new User("Lisa", "Morrison", "123456", "user"));
+        repository.deleteAll();
+        assertThat(repository.count()).isEqualTo(0);
+    }
+
+
+}
